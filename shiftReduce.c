c1b41e8da33a91e4c3d7347b2a0039c35b2192d4
Jenin82 <jenin8282@gmail.com>
2023-11-23 12:38:44 +0530

    fix

--- shiftReduce.c
+++ shiftReduce.c
@@ -14,15 +14,19 @@ void main() {
 	puts("stack \t input \t action");
 	for (i = 0; j < c;) {
 		if (a[j] == 'i' && a[j + 1] == 'd') {
+			stk[i] = 'i';
+			stk[i + 1] = 'd';
+			stk[i+2] = '\0';
+			printf("$%s\t%s$\tSHIFT->id\n", stk, a + j+2);
 			stk[i] = 'E';
 			stk[i + 1] = '\0';
-			printf("$%s\t%s$\tSHIFT->id\n", stk, a + j);
+			printf("$%s\t%s$\tREDUCE TO E\n", stk, a + j+2);
 			i++;
 			j += 2;
 		} else {
 			stk[i] = a[j];
 			stk[i + 1] = '\0';
-			printf("$%s\t%s$\tSHIFT->%c\n", stk, a + j, a[j]);
+			printf("$%s\t%s$\tSHIFT->%c\n", stk, a + j+1, a[j]);
 			i++;
 			j++;
 		}
@@ -37,7 +41,6 @@ void check() {
 	(stk[i - 2] == '+' || stk[i - 2] == '-' ||
 	stk[i - 2] == '*' || stk[i - 2] == '/') &&
 	stk[i - 3] == 'E') {
-		stk[i - 3] = 'E';
 		stk[i - 2] = '\0';
 		i -= 2;
 		printf("$%s\t%s$\tREDUCE TO E\n", stk, a + j);
